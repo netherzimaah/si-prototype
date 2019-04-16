46c699e0dc456cc67bf1c5c431baf8fc8e000240
Guilherme Zimmermann <zimaah@gmail.com>
2019-04-17 00:33:57 +0200

    First commit :)

---
+++ build/static/js/5.8f3baed7.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{1096:function(n,t,e){"use strict";function o(n,t){n.addEventListener("statusTap",function(){t.read(function(){var e=n.document.elementFromPoint(n.innerWidth/2,n.innerHeight/2);if(e){var o=e.closest("ion-content");o&&o.componentOnReady().then(function(){t.write(function(){return o.scrollToTop(300)})})}})})}e.r(t),e.d(t,"startStatusTap",function(){return o})}}]);
+//# sourceMappingURL=5.8f3baed7.chunk.js.map
